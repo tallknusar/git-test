e7be985c76497e74e9a81702a79359ce47d7f96b
tallknusar <eivind.hilde@me.com>
2017-10-11 21:30:55 +0200

    First commit

---
+++ git-test/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is main");
+		thisLabel label = new thisLabel("tekst");
+		label.printText();
+	}
+
+}
---
+++ git-test/src/thisLabel.java
@@ -0,0 +1,12 @@
+
+public class thisLabel {
+	String text;
+	public thisLabel(String text){
+		this.text = text;
+	}
+	
+	public void printText(){
+		System.out.println(text);
+	}
+
+}
